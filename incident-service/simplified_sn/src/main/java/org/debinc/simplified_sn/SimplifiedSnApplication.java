79b9da4fcf32a3d618275777e8ce33fddf707ecb
kaushikdeb <talk.kd@gmail.com>
2025-04-29 17:51:10 -0400

    spring boot starter web

---
+++ incident-service/simplified_sn/src/main/java/org/debinc/simplified_sn/SimplifiedSnApplication.java
@@ -0,0 +1,13 @@
+package org.debinc.simplified_sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedSnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedSnApplication.class, args);
+	}
+
+}
---
+++ incident-service/simplified_sn/src/test/java/org/debinc/simplified_sn/SimplifiedSnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.debinc.simplified_sn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedSnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
